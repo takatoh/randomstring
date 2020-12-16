99d92644a0b66405e30b8b3cda34198188704f58
takatoh <takatoh.m@gmail.com>
2020-12-16 09:00:59 +0900

    cmd/strrand/main.go: v0.3.0

--- cmd/strrand/main.go
+++ cmd/strrand/main.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	progName = "strrand"
-	progVersion = "v0.2.0"
+	progVersion = "v0.3.0"
 
 	uppercases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	lowercases = "abcdefghijklmnopqrstuvwxyz"
